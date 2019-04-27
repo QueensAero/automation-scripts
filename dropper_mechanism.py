499997f3bbdf8dc42b1f1602a85358a7d84a2f0c
Michael Dowling <15mhd@queensu.ca>
2019-04-27 16:48:30 -0400

    fixed pins

--- dropper_mechanism.py
+++ dropper_mechanism.py
@@ -7,14 +7,15 @@ def set_servo(pin,pwm):
 
 
 servo_low = 9
-servo_mid = 10
-servo_hi = 11
+servo_mid = 11
+servo_hi = 10
 open_pwm = 2000
 close_pwm = 1300
 
 print "dropping"
 set_servo(servo_low,open_pwm)
 Script.Sleep(500)
+print "closing"
 set_servo(servo_low,close_pwm)
 
 Script.Sleep(1000)
@@ -22,13 +23,15 @@ Script.Sleep(1000)
 print "moving down"
 set_servo(servo_mid,open_pwm)
 Script.Sleep(500)
+print "closing"
 set_servo(servo_mid,close_pwm)
 
 Script.Sleep(1000)
 
-
+print "moving down 2"
 set_servo(servo_hi,open_pwm)
 Script.Sleep(500)
+print "closing"
 set_servo(servo_hi,close_pwm)
 
 print "done"
